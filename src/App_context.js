1bbac45eeb8f0d5df4ba255b7baa7af99caf6f0b
Avijit2002 <avijitram2013@gmail.com>
2024-02-02 19:02:31 +0530

    first commit

---
+++ src/App_context.js
@@ -0,0 +1,200 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import { PostProvider,usePost } from "./PostContext";
+//import Test from './Test'
+
+
+
+function App() {
+  const [isFakeDark, setIsFakeDark] = useState(false);
+  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
+  useEffect(
+    function () {
+      document.documentElement.classList.toggle("fake-dark-mode");
+    },
+    [isFakeDark]
+  );
+
+  
+
+  return (
+      <section>
+        <button
+          onClick={() => setIsFakeDark((isFakeDark) => !isFakeDark)}
+          className="btn-fake-dark-mode"
+        >
+          {isFakeDark ? "☀️" : "🌙"}
+        </button>
+        <PostProvider>
+          <Header />
+          <Main />
+          <Archive />
+          <Footer />
+        </PostProvider>
+      </section>
+      
+  );
+}
+
+function Header() {
+  const {onClearPosts} = usePost()
+  return (
+    <header>
+      <h1>
+        <span>⚛️</span>The Atomic Blog
+      </h1>
+      <div>
+        <Results />
+        <SearchPosts />
+        <button onClick={onClearPosts}>Clear posts</button>
+      </div>
+    </header>
+  );
+}
+
+function SearchPosts() {
+  const {searchQuery, setSearchQuery} = usePost()
+  return (
+    <input
+      value={searchQuery}
+      onChange={(e) => setSearchQuery(e.target.value)}
+      placeholder="Search posts..."
+    />
+  );
+}
+
+function Results() {
+  const {posts} = usePost()
+  return <p>🚀 {posts.length} atomic posts found</p>;
+}
+
+function Main() {
+  return (
+    <main>
+      <FormAddPost />
+      <Posts />
+    </main>
+  );
+}
+
+function Posts() {
+  const {posts} = usePost()
+  return (
+    <section>
+      <List posts={posts} />
+    </section>
+  );
+}
+
+function FormAddPost() {
+  const {onAddPost} = usePost()
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const handleSubmit = function (e) {
+    e.preventDefault();
+    if (!body || !title) return;
+    onAddPost({ title, body });
+    setTitle("");
+    setBody("");
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+        placeholder="Post title"
+      />
+      <textarea
+        value={body}
+        onChange={(e) => setBody(e.target.value)}
+        placeholder="Post body"
+      />
+      <button>Add post</button>
+    </form>
+  );
+}
+
+function SlowComponent() {
+  // If this is too slow on your maching, reduce the `length`
+  const words = Array.from({ length: 100_000 }, () => "WORD");
+  return (
+    <ul>
+      {words.map((word, i) => (
+        <li key={i}>
+          {i}: {word}
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function Test({children}) {
+  const [count, setCount] = useState(0);
+  return (
+    <div>
+      <h1>Slow counter?!?</h1>
+      <button onClick={() => setCount((c) => c + 1)}>Increase: {count}</button>
+      {children}
+    </div>
+  );
+}
+
+
+function List() {
+  const {posts} = usePost()
+  return (
+    <>
+    <ul>
+      {posts.map((post, i) => (
+        <li key={i}>
+          <h3>{post.title}</h3>
+          <p>{post.body}</p>
+        </li>
+      ))}
+    </ul>
+    <Test>
+      <SlowComponent />
+    </Test>
+      </>
+  );
+}
+
+function Archive() {
+  const {onAddPost} = usePost()
+  // Here we don't need the setter function. We're only using state to store these posts because the callback function passed into useState (which generates the posts) is only called once, on the initial render. So we use this trick as an optimization technique, because if we just used a regular variable, these posts would be re-created on every render. We could also move the posts outside the components, but I wanted to show you this trick 😉
+  const [posts] = useState(() =>{}
+    // 💥 WARNING: This might make your computer slow! Try a smaller `length` first
+    //Array.from({ length: 10000 }, () => createRandomPost())
+  );
+
+  const [showArchive, setShowArchive] = useState(false);
+
+  return (
+    <aside>
+      <h2>Post archive</h2>
+      <button onClick={() => setShowArchive((s) => !s)}>
+        {showArchive ? "Hide archive posts" : "Show archive posts"}
+      </button>
+
+      {showArchive && (
+        <ul>
+          {posts.map((post, i) => (
+            <li key={i}>
+              <p>
+                <strong>{post.title}:</strong> {post.body}
+              </p>
+              <button onClick={() => onAddPost(post)}>Add as new post</button>
+            </li>
+          ))}
+        </ul>
+      )}
+    </aside>
+  );
+}
+
+memo(function Footer() {
+  return <footer>&copy; by The Atomic Blog ✌️</footer>;
+})
+
+export default App;
